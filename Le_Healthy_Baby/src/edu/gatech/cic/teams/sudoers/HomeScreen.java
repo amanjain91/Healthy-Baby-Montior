d20616fb22df09528ef10fbd2424c64b81d34cb4
Surenkumar Nihalani <suren@gatech.edu>
2012-03-07 23:32:30 -0500

    Starting with home screen.

--- Le_Healthy_Baby/src/edu/gatech/cic/teams/sudoers/HomeScreen.java
+++ Le_Healthy_Baby/src/edu/gatech/cic/teams/sudoers/HomeScreen.java
@@ -3,7 +3,7 @@ package edu.gatech.cic.teams.sudoers;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class LoginScreen extends Activity {
+public class HomeScreen extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
