34cd3ee431c8c5a8369eda7bd3f3290158665048
Surenkumar Nihalani <suren@gatech.edu>
2012-03-08 00:35:46 -0500

    End of day commit. Implemented the layout for the home screen, adding a child. Implemented the button listener for add child.

--- Le_Healthy_Baby/gen/edu/gatech/cic/teams/sudoers/R.java
+++ Le_Healthy_Baby/gen/edu/gatech/cic/teams/sudoers/R.java
@@ -14,9 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button_log_in=0x7f050002;
-        public static final int edit_text_username=0x7f050000;
-        public static final int enter_password=0x7f050001;
+        public static final int button_add_child=0x7f050000;
+        public static final int button_log_in=0x7f050003;
+        public static final int editText1=0x7f050004;
+        public static final int edit_text_username=0x7f050001;
+        public static final int enter_password=0x7f050002;
+        public static final int radio_button_female=0x7f050006;
+        public static final int radio_button_male=0x7f050005;
     }
     public static final class layout {
         public static final int homescreen_layout=0x7f030000;
