70776904a5dfd71d4675b263b5ea670148492352
Gurden Batra <gurden.batra@gatech.edu>
2012-03-11 19:01:06 -0400

    Added Vaccination stuff.

---
+++ Le_Healthy_Baby/src/edu/gatech/cic/teams/sudoers/VaccinationScreen.java
@@ -0,0 +1,16 @@
+package edu.gatech.cic.teams.sudoers;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class VaccinationScreen extends Activity {
+	private LinearLayout mMainLayout;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(mMainLayout);
+	}
+
+}
