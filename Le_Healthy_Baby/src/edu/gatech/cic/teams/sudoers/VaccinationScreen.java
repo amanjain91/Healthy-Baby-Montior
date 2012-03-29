e9b4f77845a9d4846737bfb38a4b52ce1d485f90
Surenkumar Nihalani <suren@gatech.edu>
2012-03-28 21:42:23 -0400

    Vaccination Screen works now.

--- Le_Healthy_Baby/src/edu/gatech/cic/teams/sudoers/VaccinationScreen.java
+++ Le_Healthy_Baby/src/edu/gatech/cic/teams/sudoers/VaccinationScreen.java
@@ -14,23 +14,24 @@ public class VaccinationScreen extends Activity {
 	private int mm, yy;
 	private static String[][] chart = VaccinationData.getChart();
 	private static ArrayList<String> vList;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		vList = new ArrayList<String>();
 		initLayout();
+		makeVaccineList();
 		setContentView(mMainLayout);
 	}
-	
-	private void initLayout() {	
+
+	private void initLayout() {
 		mMainLayout = new LinearLayout(getApplicationContext());
 		mMainLayout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
 				LayoutParams.FILL_PARENT));
 		mMainLayout.setOrientation(LinearLayout.VERTICAL);
-		
-		
+
 	}
-	
+
 	public void makeVaccineList() {
 		mMainLayout.removeAllViews();
 		TextView temp;
@@ -39,82 +40,83 @@ public class VaccinationScreen extends Activity {
 		Calendar cal = Calendar.getInstance();
 		int month = cal.get(Calendar.MONTH) + 1;
 		int year = cal.get(Calendar.YEAR);
-		
-		int childYear = year-yy;
+
+		int childYear = year - yy;
 		int childMonth;
-		
-		if(childYear < 2){
+
+		if (childYear < 2) {
 			childMonth = month - mm;
 		}
-		
-		else{
+
+		else {
 			childMonth = 0;
 		}
-		
-		if(childYear == 0){
-			if(childMonth == 0){
+
+		if (childYear == 0) {
+			if (childMonth == 0) {
 				addToVList(0);
 			}
-			
-			else if(childMonth == 1){
+
+			else if (childMonth == 1) {
 				addToVList(1);
 			}
-			
-			else if(childMonth > 1 && childMonth <= 3){
+
+			else if (childMonth > 1 && childMonth <= 3) {
 				addToVList(2);
 			}
-			
-			else if(childMonth > 3 && childMonth <= 5){
+
+			else if (childMonth > 3 && childMonth <= 5) {
 				addToVList(3);
 			}
-			
-			else if(childMonth > 5 && childMonth <= 8){
+
+			else if (childMonth > 5 && childMonth <= 8) {
 				addToVList(4);
 			}
-			
-			else if(childMonth > 8 && childMonth <= 11){
+
+			else if (childMonth > 8 && childMonth <= 11) {
 				addToVList(5);
 			}
 		}
-		
-		else if(childYear == 1){
-			if(childMonth >= 0 && childMonth <= 2){
+
+		else if (childYear == 1) {
+			if (childMonth >= 0 && childMonth <= 2) {
 				addToVList(6);
 			}
-			
-			else if(childMonth > 2 && childMonth <= 5){
+
+			else if (childMonth > 2 && childMonth <= 5) {
 				addToVList(7);
 			}
-			
-			else if(childMonth == 6){
+
+			else if (childMonth == 6) {
 				addToVList(8);
 			}
-			
-			else if(childMonth > 6 && childMonth <= 11){
+
+			else if (childMonth > 6 && childMonth <= 11) {
 				addToVList(9);
 			}
 		}
-		
-		else if(childYear > 1 && childYear <= 3){
+
+		else if (childYear > 1 && childYear <= 3) {
 			addToVList(10);
 		}
-		
-		else{
+
+		else {
 			addToVList(11);
 		}
-		
-		for(int i =0 ; i < vList.size(); i++){
+
+		for (int i = 0; i < vList.size(); i++) {
 			temp = new TextView(getApplicationContext());
 			temp.setText(vList.get(i));
-			temp.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+			temp.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
+					LayoutParams.WRAP_CONTENT));
 			mMainLayout.addView(temp);
 		}
-		
+
 	}
-	
-	public static void addToVList(int c){
-		for(int i = 0; i < 11;i++){
-			if(chart[i][c].compareTo("0") != 0){
+
+	public static void addToVList(int c) {
+		for (int i = 0; i < 11; i++) {
+			if (chart[i][c].compareTo("0") != 0) {
 				vList.add(chart[i][c]);
 			}
 		}
