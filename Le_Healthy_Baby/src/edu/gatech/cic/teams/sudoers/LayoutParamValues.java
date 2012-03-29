c3aaca2aa2c68d1485f989901f4e0d7d3d8bcb30
Surenkumar Nihalani <suren@gatech.edu>
2012-03-28 22:33:49 -0400

    Commited my testing AndroidManifest.xml. Reverted Back.

---
+++ Le_Healthy_Baby/src/edu/gatech/cic/teams/sudoers/LayoutParamValues.java
@@ -0,0 +1,8 @@
+package edu.gatech.cic.teams.sudoers;
+
+import android.view.ViewGroup.LayoutParams;
+
+public class LayoutParamValues {
+	public static final LayoutParams BOTH_FILL_PARENT = new LayoutParams(
+			LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+}
